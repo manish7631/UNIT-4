59e282f1a6d1bd6349b7cec09e470fb7887681f1
manishfw13 <manish_fw13_347@masai.school>
2022-03-03 12:44:49 +0530

    first commit

---
+++ divided.js
@@ -0,0 +1,5 @@
+function divided(a, b){
+    console.log(a / b)
+}
+
+module.exports = divided
---
+++ main.js
@@ -0,0 +1,12 @@
+const Add = require("./add")
+Add(6, 3)
+
+const Subtract = require("./subtract")
+Subtract(6, 3)
+
+const Multiply = require("./multiply")
+Multiply(6, 3)
+
+const Divided = require("./divided")
+Divided(6, 3)
+
